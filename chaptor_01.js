1fdb3b1a356742e271d38a12e7f6926857ef745e
kdj <rlaehdwp7451@gmail.com>
2020-09-26 22:10:28 +0900

    create totalVolumeCredits

--- chaptor_01.js
+++ chaptor_01.js
@@ -23,16 +23,14 @@ const invoices =
     ]
 };
 
-function statement(invoice, plays) {
+function statement(invoice) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
         result += `   ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
+    let volumeCredits = totalVolumeCredits(invoice);
 
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
@@ -72,6 +70,14 @@ function volumeCreditsFor(aPerformance) {
         result += Math.floor(aPerformance.audience / 5)
     };
     return result;
+}
+
+function totalVolumeCredits(invoice){
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
 }
 
 function usd(aNumber){
