66813bfc189ae54304b07d5f173b9396ce54006b
kdj <rlaehdwp7451@gmail.com>
2020-09-26 21:43:34 +0900

    delete play argument

--- chaptor_01.js
+++ chaptor_01.js
@@ -35,7 +35,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf,playFor(perf));
+        let thisAmount = amountFor(perf);
 
         volumeCredits += Math.max(perf.audience - 30, 0);
         if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
@@ -48,9 +48,9 @@ function statement(invoice, plays) {
     return result;
 }
 
-function amountFor(aPerformance, play) {
+function amountFor(aPerformance) {
     let result = 0;
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
         case "tragedy":
             result = 40000;
             if (aPerformance.audience > 30) {
@@ -65,7 +65,7 @@ function amountFor(aPerformance, play) {
             result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
+            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
     }
     return result;
 }
