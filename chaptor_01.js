138d64784245da3e7134b4d73abbe2245c56e7e4
kdj <rlaehdwp7451@gmail.com>
2020-09-26 21:54:46 +0900

    delete format variable

--- chaptor_01.js
+++ chaptor_01.js
@@ -28,17 +28,12 @@ function statement(invoice, plays) {
     let volumeCredits = 0;
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
 
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format;
-
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
         result += `   ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
+    
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
@@ -77,6 +72,14 @@ function volumeCreditsFor(aPerformance) {
         result += Math.floor(aPerformance.audience / 5)
     };
     return result;
+}
+
+function format(aNumber){
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2
+    }).format(aNumber);
 }
 
 console.log(statement(invoices, plays));
