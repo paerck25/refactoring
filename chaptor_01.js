7237f24e9af0ec21620a0933b21fc3e12b159e97
kdj <rlaehdwp7451@gmail.com>
2020-09-26 22:43:47 +0900

    create renderPlainText

--- chaptor_01.js
+++ chaptor_01.js
@@ -24,72 +24,79 @@ const invoices =
 };
 
 function statement(invoice) {
+    return renderPlainText(invoice);
+}
+
+function renderPlainText(invoice) {
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
     for (let perf of invoice.performances) {
         result += `   ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
-    result += `총액: ${usd(totalAmount(invoice))}\n`;
-    result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
-}
 
-function amountFor(aPerformance) {
-    let result = 0;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    function amountFor(aPerformance) {
+        let result = 0;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        }
+        return result;
     }
-    return result;
-}
 
-function playFor(aPerformance) {
-    return plays[aPerformance.playID]
-}
-
-function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type) {
-        result += Math.floor(aPerformance.audience / 5)
-    };
-    return result;
-}
-
-function totalVolumeCredits(invoice){
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID]
     }
-    return volumeCredits;
-}
 
-function usd(aNumber){
-    return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format(aNumber/100);
-}
-
-function totalAmount(invoice){
-    let totalAmount = 0;
-    for (let perf of invoice.performances) {
-        totalAmount += amountFor(perf);
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type) {
+            result += Math.floor(aPerformance.audience / 5)
+        };
+        return result;
     }
-    return totalAmount;
+
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+            minimumFractionDigits: 2
+        }).format(aNumber / 100);
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
 }
+
+
+
 
 console.log(statement(invoices, plays));
