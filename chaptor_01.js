4831ca186ada248be74518cb009dbc47e173badb
kdj <rlaehdwp7451@gmail.com>
2020-09-26 21:56:48 +0900

    change foramt -> usd

--- chaptor_01.js
+++ chaptor_01.js
@@ -30,11 +30,11 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
-        result += `   ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        result += `   ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    
-    result += `총액: ${format(totalAmount / 100)}\n`;
+
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
@@ -74,12 +74,12 @@ function volumeCreditsFor(aPerformance) {
     return result;
 }
 
-function format(aNumber){
+function usd(aNumber){
     return new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD",
         minimumFractionDigits: 2
-    }).format(aNumber);
+    }).format(aNumber/100);
 }
 
 console.log(statement(invoices, plays));
