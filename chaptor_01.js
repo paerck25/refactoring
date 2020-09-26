0fa3246e04117281778ba1a206a8d8d0f22ec771
kdj <rlaehdwp7451@gmail.com>
2020-09-26 21:51:10 +0900

    create volumeCreditsFor

--- chaptor_01.js
+++ chaptor_01.js
@@ -35,10 +35,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-
+        volumeCredits += volumeCreditsFor(perf);
         result += `   ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
@@ -69,8 +66,16 @@ function amountFor(aPerformance) {
     return result;
 }
 
-function playFor(aPerformance){
+function playFor(aPerformance) {
     return plays[aPerformance.playID]
+}
+
+function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type) 
+        volumeCredits += Math.floor(perf.audience / 5);
+    return volumeCredits;
 }
 
 console.log(statement(invoices, plays));
