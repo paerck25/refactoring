215d52e1455d194ea8bcc3b7bda14017177ada06
kdj <rlaehdwp7451@gmail.com>
2020-09-26 21:41:18 +0900

    delete play variable

--- chaptor_01.js
+++ chaptor_01.js
@@ -35,13 +35,12 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        const play = playFor(perf);
-        let thisAmount = amountFor(perf,play);
+        let thisAmount = amountFor(perf,playFor(perf));
 
         volumeCredits += Math.max(perf.audience - 30, 0);
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
-        result += `   ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+        result += `   ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
     result += `총액: ${format(totalAmount / 100)}\n`;
